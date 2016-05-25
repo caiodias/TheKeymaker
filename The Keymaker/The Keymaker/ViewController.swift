7d7c6fc450394ff5c5b6c6506e19a0da81a9cc08
Caio Dias <kiobreno@gmail.com>
2016-05-25 14:20:22 -0400

    First step, a little step.

---
+++ The Keymaker/The Keymaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Keymaker
+//
+//  Created by Caio Dias on 2016-05-24.
+//  Copyright © 2016 Caio Dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ The Keymaker/The KeymakerTests/The_KeymakerTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_KeymakerTests.swift
+//  The KeymakerTests
+//
+//  Created by Caio Dias on 2016-05-24.
+//  Copyright © 2016 Caio Dias. All rights reserved.
+//
+
+import XCTest
+@testable import The_Keymaker
+
+class The_KeymakerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
